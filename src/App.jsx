1fb99823b7bfb5de14416d28658223e292454e0b
Poongothai-R <poongothai.rajesh@icloud.com>
2023-03-30 15:22:37 +0200

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import "../src/styles/global/app.css"
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Unlimited films, TV programmes and more.</h1>
+    </div>
+  );
+}
+
